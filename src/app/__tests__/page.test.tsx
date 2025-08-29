3f6ff3832516dbd6c99814c68e46c042da5fdada
Eric Zhang <superezzhang@gmail.com>
2025-08-28 23:58:24 -0400

    test: lobby integration test

---
+++ src/app/__tests__/page.test.tsx
@@ -0,0 +1,15 @@
+import { describe, expect, it } from "vitest";
+import Home from "../page";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("Lobby Tests", () => {
+  it("should load the lobby on initial page load", () => {
+    render(<Home></Home>);
+
+    const title = screen.getByText("AI Pictionary");
+    const startGameButton = screen.getByText("Start Game!");
+
+    expect(title).toBeInTheDocument();
+  });
+});
