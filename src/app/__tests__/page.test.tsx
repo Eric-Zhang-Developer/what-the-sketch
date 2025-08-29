5129daaa95a06db3e444fe09b5677e377406cfcf
Eric Zhang <superezzhang@gmail.com>
2025-08-29 00:33:28 -0400

    test: add test for lobby check

--- src/app/__tests__/page.test.tsx
+++ src/app/__tests__/page.test.tsx
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest";
 import Home from "../page";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/vitest";
 
 describe("Lobby Tests", () => {
   it("should load the lobby on initial page load", () => {
@@ -11,5 +12,6 @@ describe("Lobby Tests", () => {
     const startGameButton = screen.getByText("Start Game!");
 
     expect(title).toBeInTheDocument();
+    expect(startGameButton).toBeInTheDocument();
   });
 });
