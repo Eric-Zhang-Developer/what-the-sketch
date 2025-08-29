af8c2d1408fd189c30444f3f68c8fb0caf44eeec
Eric Zhang <superezzhang@gmail.com>
2025-08-29 00:53:55 -0400

    test: add integration test to check if title and button are gone after click

--- src/app/__tests__/page.test.tsx
+++ src/app/__tests__/page.test.tsx
@@ -1,8 +1,9 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, afterEach } from "vitest";
 import Home from "../page";
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/vitest";
+afterEach(() => cleanup());
 
 describe("Lobby Tests", () => {
   it("should load the lobby on initial page load", () => {
@@ -13,5 +14,17 @@ describe("Lobby Tests", () => {
 
     expect(title).toBeInTheDocument();
     expect(startGameButton).toBeInTheDocument();
+  });
+
+  it("should not show the title and start button after the use presses start game", async () => {
+    render(<Home></Home>);
+    const user = userEvent.setup();
+
+    const title = screen.getByText("AI Pictionary");
+    const startGameButton = screen.getByText("Start Game!");
+    await user.click(startGameButton);
+
+    expect(title).not.toBeInTheDocument();
+    expect(startGameButton).not.toBeInTheDocument();
   });
 });
