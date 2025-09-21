ee7412873612aa6cfcc660202de92b39c526debe
Eric Zhang <superezzhang@gmail.com>
2025-09-20 23:34:57 -0400

    feat: update styling of the results box

--- src/components/TurnResultSection.tsx
+++ src/components/TurnResultSection.tsx
@@ -6,6 +6,11 @@ export default function TurnResultSection({ onNextPromptClick }: TurnResultProps
   const guessState = useGameStore((state) => state.guessState);
   const response = useGameStore((state) => state.response);
 
+  const parts = response.split("My guess is ");
+
+  const commentary = parts[0];
+  const guess = parts[1];
+
   // Visual Indicator for guess correctness, this is a placeholder for a more advanced scoring system
   const borderColorMap = {
     [GuessState.Pending]: "border-black",
@@ -16,10 +21,15 @@ export default function TurnResultSection({ onNextPromptClick }: TurnResultProps
   return (
     <>
       <div
-        className={`border-2 px-60 py-10 rounded-2xl mt-10 ${borderColorMap[guessState]}`}
+        className={`border-2 py-10 px-10 rounded-2xl text-center mt-10 bg-slate-100 shadow-xl flex flex-col gap-4 ${borderColorMap[guessState]}`}
         data-testid="turn-result-section"
       >
-        <Markdown>{response}</Markdown>
+        <div className="text-lg text-slate-800">
+          <Markdown>{commentary}</Markdown>
+        </div>
+        <div className="text-2xl">
+          <Markdown>{guess}</Markdown>
+        </div>
       </div>
 
       <button
