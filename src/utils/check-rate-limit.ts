685c38041824fa6a7f400d476a6ba1b8ab6a1e5d
Eric Zhang <superezzhang@gmail.com>
2025-11-06 23:50:43 -0500

    docs: plan out rate limiting

--- src/utils/check-rate-limit.ts
+++ src/utils/check-rate-limit.ts
@@ -1,13 +1,26 @@
+import "server-only";
 import { createClient } from "@/utils/supabase/server";
 
-// IP based rate limiting with supabase
-// We call the ip_rate_limits database and check that ip's rate limit
+const RATE_LIMIT_COUNT = 50;
+const RATE_LIMIT_WINDOW = 24 * 60 * 60 * 1000; // 24 hours
 
 export async function checkRateLimit(ip: string) {
   const supabase = await createClient();
+  const now = Date.now();
 
-  const { data: user_rate_limit } = await supabase.from("ip_rate_limits").select("*").eq("ip", ip);
+  // Select data from the database
 
-  // For now hard coded to false
+  // if it fails lock user out just to be safe
+
+  // ip data:
+
+  // If we have a user
+  //  --- Case 1: User is within the 24 hour window --
+  //    --- If user is within rate limit update their count by 1
+  //  --- Case 2: User is out of the 24 hour window --
+
+  // No ip in database:
+  //  --- Case 3: User is not in database --- Add User
+
   return { limited: false };
 }
