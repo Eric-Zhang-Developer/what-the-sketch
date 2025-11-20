36ff60acc6760d56daf3ad0a51d4bdb3384d4a1c
Eric Zhang <superezzhang@gmail.com>
2025-11-20 15:46:24 -0500

    feat: replace AI Pictionary Title with What the Sketch? banner

--- src/components/Lobby.tsx
+++ src/components/Lobby.tsx
@@ -1,5 +1,6 @@
 import { useGameStore } from "@/store/gameStore";
 import { GameState } from "@/utils/types";
+import Image from "next/image";
 
 export default function Lobby() {
   const setGameState = useGameStore((state) => state.setGameState);
@@ -9,8 +10,15 @@ export default function Lobby() {
   };
 
   return (
-    <main className="container mx-auto flex flex-col items-center gap-10 justify-center pt-40">
-      <h1 className="text-5xl">AI Pictionary</h1>
+    <main className="container mx-auto flex flex-col items-center gap-10 justify-center pt-20">
+      <Image
+        src="/banner.webp"
+        alt="What the Sketch? Banner"
+        width={900}
+        height={314}
+        className="w-full max-w-3xl h-auto"
+        priority
+      ></Image>
       <button
         onClick={handleClick}
         className="bg-blue-400 text-white px-10 py-3 rounded-xl text-2xl shadow-lg hover:cursor-pointer transition hover:scale-110"
