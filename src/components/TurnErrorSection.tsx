7a99960f59654d060d4599cec542da9d57cbc3d1
Eric Zhang <superezzhang@gmail.com>
2025-12-23 21:01:50 -0500

    style: style error section

--- src/components/TurnErrorSection.tsx
+++ src/components/TurnErrorSection.tsx
@@ -1,10 +1,25 @@
 import { useGameStore } from "@/store/gameStore";
+import Button from "./Button";
+import { TurnCycleState } from "@/utils/types";
+import { TriangleAlert } from "lucide-react";
 export default function TurnErrorSection() {
   const errorMessage = useGameStore((state) => state.errorMessage);
+  const setTurnCycleState = useGameStore((state) => state.setTurnCycleState);
+
+  const handleTryAgain = () => {
+    setTurnCycleState(TurnCycleState.Drawing);
+  };
   return (
-    <>
-      <div>Error</div>
-      <p>{errorMessage}</p>
-    </>
+    <section
+      className="flex flex-col justify-center items-center border-4 p-8 rounded-2xl text-center bg-white border-red-500 gap-4 "
+      style={{
+        boxShadow: `4px 6px 0px 0px rgba(239, 68, 68, 1)`,
+      }}
+    >
+      <TriangleAlert size={48}></TriangleAlert>
+      <span className="text-2xl">Whoops!</span>
+      <p className="text-xl">{errorMessage}</p>
+      <Button onClick={handleTryAgain}>Try Again</Button>
+    </section>
   );
 }
