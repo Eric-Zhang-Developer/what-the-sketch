044c65eea6c11f383ebd24ee1b4b0f32d073753e
Eric Zhang <superezzhang@gmail.com>
2026-01-01 23:05:56 -0500

    feat: switch to openrouter

--- src/app/api/generate-response/route.ts
+++ src/app/api/generate-response/route.ts
@@ -3,6 +3,7 @@ import { ipAddress } from "@vercel/functions";
 import { GoogleGenAI } from "@google/genai";
 import { checkRateLimit } from "@/utils/check-rate-limit";
 import z from "zod";
+import { OpenRouter } from "@openrouter/sdk";
 
 export async function POST(request: NextRequest) {
   // --- IP rate limiting check ---
@@ -34,31 +35,42 @@ export async function POST(request: NextRequest) {
   if (!result.success) {
     return NextResponse.json({ error: "Invalid request" }, { status: 400 });
   }
-  const image = result.data.image;
+  const image = `data:image/png;base64,${result.data.image}`;
 
   // --- Gemini API Call ---
   try {
     // To-do: For future reference for changeable prompts also feed a textPrompt. variable to contents
-    const contents = [
-      {
-        inlineData: {
-          mimeType: "image/png",
-          data: image,
-        },
-      },
-      {
-        text: `You are an enthusiastic and slightly sassy AI game partner in a Pictionary-style game. Your goal is to analyze a user's drawing and guess what it is. 
-        First, provide a playful, and slightly roasting commentary on the drawing's quality (keep it lighthearted and fun). 
-        Then, you MUST end your response with your final guess in the following format, and nothing else after it: " My guess is **WORD**". `,
-      },
-    ];
-    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY! });
-    const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: contents,
+
+    const openRouter = new OpenRouter({
+      apiKey: process.env.OPENROUTER_API_KEY!,
     });
+
+    const response = await openRouter.chat.send({
+      model: "google/gemini-2.5-flash",
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text: `You are an enthusiastic and slightly sassy AI game partner in a Pictionary-style game. Your goal is to analyze a user's drawing and guess what it is. 
+                    First, provide a playful, and slightly roasting commentary on the drawing's quality (keep it lighthearted and fun). 
+                    Then, you MUST end your response with your final guess in the following format, and nothing else after it: " My guess is **WORD**". `,
+            },
+            {
+              type: "image_url",
+              imageUrl: {
+                url: image,
+              },
+            },
+          ],
+        },
+      ],
+      stream: false,
+    });
+
     // Return only text for now all the metadata I don't want to expose to client side
-    return NextResponse.json({ response: response.text });
+    return NextResponse.json({ response: response.choices[0].message.content });
   } catch (error) {
     console.error("Gemini API error:", error);
     return NextResponse.json({ error: `AI API service unavailable` }, { status: 500 });
